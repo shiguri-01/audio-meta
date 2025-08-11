a7a0d68adf75678227a51a36478eb701d40a227b
shiguri <shiguri01.gm@gmail.com>
2025-08-11 17:23:40 +0900

    feat: Tauriをセットアップ

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,4 +12,32 @@ export default defineConfig({
     solidPlugin(),
     tailwindcss(),
   ],
+
+  // ViteがRustのエラーを不明瞭にしないようにする
+  clearScreen: false,
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  // これらのプレフィックスで始まる環境変数は、フロントエンドで`import.meta.env`からアクセスできる
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+  build: {
+    // TauriはWindowsではChromium、macOS・LinuxではWebKitを使用
+    target:
+      process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13",
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 });
