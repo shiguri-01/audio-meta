dfcd5ddb952050c42f18121cc6bb5ad296bdf133
shiguri <shiguri01.gm@gmail.com>
2025-08-23 13:17:35 +0900

    feat: standard-schemaを消去

--- src/tauri/utils.ts
+++ src/tauri/utils.ts
@@ -1,7 +1,4 @@
-import type { StandardSchemaV1 } from "@standard-schema/spec";
 import type { Result } from "neverthrow";
 
 export type toDTO<T, DTO> = (input: T) => DTO;
-export type fromDTO<T, DTO> = (
-  dto: DTO,
-) => Result<T, readonly StandardSchemaV1.Issue[]>;
+export type fromDTO<T, DTO> = (dto: DTO) => Result<T, string[]>;
