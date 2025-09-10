6419831db55ac5f9b4167522bdec522f4854285e
shiguri <shiguri01.gm@gmail.com>
2025-09-10 09:26:46 +0900

    feat: 外部で使うAudioFileStore関連の機能をエクスポート

--- src/features/audio-file/index.ts
+++ src/features/audio-file/index.ts
@@ -7,6 +7,10 @@ export * from "./components/DirectorySelector";
 // Providers
 export * from "./providers/audio-files-manager-provider";
 export type { AudioFilesManager } from "./primitives/audio-files-manager";
+export {
+  type AudioFileStore,
+  AudioFileStoreProvider,
+} from "./primitives/audio-file-store";
 
 // Schemas
 export * from "./schemas";
